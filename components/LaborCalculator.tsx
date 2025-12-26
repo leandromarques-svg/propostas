f23bd78a4a8b853a57a2d6a79d0a32ce51bd6f0f
leandromarques-svg <leandromarques@metarh.com.br>
2025-12-26 17:53:22 -0300

    correção da calculadora-VT

--- components/LaborCalculator.tsx
+++ components/LaborCalculator.tsx
@@ -2685,6 +2685,12 @@ export const LaborCalculator: React.FC<LaborCalculatorProps> = ({ onCancel }) =>
                                         </div>
                                     )}
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
